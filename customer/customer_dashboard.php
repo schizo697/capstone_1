c151bc265f2f78c57184da568af48d2057779f2e
Ann <fernandez.226952@gensan.sti.edu.ph>
2024-05-10 19:42:05 +0800

    sdds

--- customer/customer_dashboard.php
+++ customer/customer_dashboard.php
@@ -3,6 +3,40 @@ import { Ripple, initMDB } from "mdb-ui-kit";
 
 initMDB({ Ripple });</script>
 
+<style>
+.card {
+  width: 18rem;
+}
+
+.card .bg-image {
+  position: relative;
+  overflow: hidden;
+}
+
+.card .bg-image img {
+  width: 100%;
+  height: 200px; /* Adjust the height as needed */
+  object-fit: cover;
+}
+
+.card .card-body {
+  padding: 1.25rem;
+}
+
+.card .card-title {
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.card .card-text {
+  margin-bottom: 1rem;
+}
+
+.card .btn-primary {
+  width: 100%;
+}
+</style>
+
 <div class="card">
   <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
     <img src="assets/dist/img/photo1.png" class="img-fluid"/>
--- farmer/includes/sidebar.php
+++ farmer/includes/sidebar.php
@@ -96,3 +96,14 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.2.4/js/bootstrap.bundle.min.js"></script>
+
+<script>
+  $(document).ready(function() {
+    // Enable the dropdown functionality for the sidebar
+    $('.sidebar .nav-treeview').slideUp(0);
+    $('.sidebar .nav-item .nav-link').on('click', function() {
+      $(this).parent().siblings().find('.nav-treeview').slideUp();
+      $(this).next('.nav-treeview').slideToggle();
+    });
+  });
+</script>
