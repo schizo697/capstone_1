73e3759a6909bfb7adadc2fdd0b88d70a8ee52f2
schizo697 <mynftaccount269@gmail.com>
2024-05-11 11:19:03 +0900

    profile

--- admin/admin_profile.php
+++ admin/admin_profile.php
@@ -2,56 +2,11 @@
 include('includes/header.php');
 include('includes/topbar.php');
 include('includes/sidebar.php');
+include('includes/admin_profile.php');
 ?>
 <script>https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css</script>
 <script>https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js</script>
 <script>https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js</script>
-<style>
-    body {
-    background: rgb(128, 128, 128)
-}
-
-.form-control:focus {
-    box-shadow: none;
-    border-color: #BA68C8
-}
-
-.profile-button {
-    background: rgb(99, 39, 120);
-    box-shadow: none;
-    border: none
-}
-
-.profile-button:hover {
-    background: #682773
-}
-
-.profile-button:focus {
-    background: #682773;
-    box-shadow: none
-}
-
-.profile-button:active {
-    background: #682773;
-    box-shadow: none
-}
-
-.back:hover {
-    color: #682773;
-    cursor: pointer
-}
-
-.labels {
-    font-size: 11px
-}
-
-.add-experience:hover {
-    background: #BA68C8;
-    color: #fff;
-    cursor: pointer;
-    border: solid 1px #BA68C8
-}
-</style>
 <div class="container rounded bg-white mt-5 mb-5">
     <div class="row">
         <div class="col-md-3 border-right">
@@ -62,32 +17,47 @@ include('includes/sidebar.php');
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h4 class="text-right">Profile Settings</h4>
                 </div>
-                <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                    <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
-                    <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value=""></div>
-                    <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                    <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                    <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                    <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                    <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value=""></div>
-                    <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value=""></div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control" placeholder="country" value=""></div>
-                    <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state"></div>
-                </div>
-                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="p-3 py-5">
-                <div class="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span class="border px-3 p-1 add-experience"><i class="fa fa-plus"></i>&nbsp;Experience</span></div><br>
-                <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value=""></div> <br>
-                <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value=""></div>
+                <?php 
+                if(isset($_SESSION['user_id'])){
+                    $user_id = $_SESSION['user_id'];
+
+                    $profileSql = "SELECT firstname, lastname, gender, contact, address FROM user_info WHERE info_id = '$user_id'";
+                    $profileResult = mysqli_query($conn, $profileSql);
+
+                    if($profileResult && mysqli_num_rows($profileResult) > 0){
+                        $profileRow = mysqli_fetch_assoc($profileResult);
+                        ?>
+                        <form>
+                        <div class="row mt-2">
+                            <div class="col-md-6">
+                                <label class="labels">Firstname</label>
+                                <input type="text" class="form-control" value="<?php echo $profileRow['firstname'] ?>">
+                            </div>
+                            <div class="col-md-6">
+                                <label class="labels">Lastname</label>
+                                <input type="text" class="form-control" value="<?php echo $profileRow['lastname'] ?>">
+                            </div>
+                            <div class="col-md-6">
+                                <label class="labels">Gender</label>
+                                <input type="text" class="form-control" value="<?php echo $profileRow['gender'] ?>">
+                            </div>
+                            <div class="col-md-6">
+                                <label class="labels">Contact</label>
+                                <input type="text" class="form-control" value="<?php echo $profileRow['contact'] ?>">
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-12">
+                                <label class="labels">Address</label>
+                                <input type="text" class="form-control" value="<?php echo $profileRow['address'] ?>">
+                            </div>
+                        </div>
+                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                        </form>
+                        <?php
+                    }
+                }
+                ?>
             </div>
         </div>
     </div>
