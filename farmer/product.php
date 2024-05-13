8db4a3c8e64a412e505af962154877a33ac5c7f5
schizo697 <mynftaccount269@gmail.com>
2024-05-14 01:13:59 +0900

    aa

--- farmer/product.php
+++ farmer/product.php
@@ -69,7 +69,7 @@ include('includes/navbar.php');
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Add Products</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
